871dd6cd686b0d2aa4ee06a7e1d2e311479a99f8
ViviRodri2010 <146996438+ViviRodri2010@users.noreply.github.com>
2023-12-02 03:19:40 +0000

    Traffic light

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,46 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const lightClicked = (e) => {
+    setActiveLight(e.target.className);
+}
+
+
+const classNames = ["red", "yellow", "green"];
+
+const activeLights = [0, 0, 0];
+
+const setActiveLight = (className) => {
+    for(let i = 0; i < activeLights.length; i++){
+        activeLights[i] = classNames[i] == className ? activeLights[i] = 1 : activeLights[i] = 0;
+    }
+    rend();
+}
+
+function doLights(value){
+    return value == 1 ? "lit" : "";
+}
+window.onload = rend();
+function rend(){
+    
+    ReactDOM.render(
+    <>
+    
+        <div className="PALO"></div>
+        <div className="lights">
+            <div className="light">
+                <div className="red" id={doLights(activeLights[0])} onClick={lightClicked}></div>
+            </div>
+            <div className="light">
+                <div className="yellow" id={doLights(activeLights[1])} onClick={lightClicked}></div>
+            </div>
+            <div className="light">
+                <div className="green" id={doLights(activeLights[2])} onClick={lightClicked}>
+            </div></div>
+            
+        </div>
+        
+    </>, document.querySelector("#app"));
+}
+
